ec82a7213c4a22259d6f14cebd50793b11ffd91f
Rokhaya-caty19 <118398575+Rokhaya-caty19@users.noreply.github.com>
2022-11-28 17:25:45 +0100

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,16 @@
+</script>
+</head>
+<body>
+    <h1></h1>
+    <form name="CALC">
+        <input type="text"name="nbr1" id="n1"placeholder="entrer le premier nombre"/><br /><br>
+        <input type="text"name="nbr2" id="n2"placeholder="entrer le second nombre"/><br /><br>
+        <input Type ="button" value="+" onclick="operation('addition')"/>
+        <input Type ="button" value="-" onclick="operation('soustraction')"/>
+        <input Type ="button" value="*" onclick="operation('multiplication')"/>
+        <input Type ="button" value="/" onclick="operation('division')"/>
+<br/><br>
+        <input type="text" id="n3" name="res"/>
+ </form>
+</body>
+</html>
